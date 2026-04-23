80b4e476f1912222f5fa66cf3b1e8e786df52502
nehab0405 <nehabazaar@gmail.com>
2026-04-23 15:25:54 +0530

    Update App.java

--- src/main/java/com/bnmit/App.java
+++ src/main/java/com/bnmit/App.java
@@ -1,24 +1,12 @@
 package com.bnmit;
 
 
-public class App {
-    public static void main(String[] args) {
-        double balance = 20000.0;
-
-        System.out.println("===== BANK APPLICATION =====");
-        System.out.println("Initial Balance: " + balance);
-
-        double deposit = 900.0;
-        balance += deposit;
-        System.out.println("Deposited: " + deposit);
-
-        double withdraw = 300.0;
-        if (withdraw <= balance) {
-            balance -= withdraw;
-            System.out.println("Withdrawn: " + withdraw);
-        }
-
-        System.out.println("Final Balance: " + balance);
-        System.out.println("Bank operation completed successfully.");
+class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
     }
 }
+
